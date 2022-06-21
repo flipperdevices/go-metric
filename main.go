0b7f467870e96966eb47cb509eba07b0ad491da8
LionZXY <nikita@kulikof.ru>
2022-06-21 11:15:09 +0400

    Add and test basic server

--- main.go
+++ main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"fmt"
+	pb "github.com/flipperdevices/go-metric/internal/proto"
+	"google.golang.org/protobuf/proto"
+	"io"
+	"log"
 	"net/http"
 )
 
 func report(w http.ResponseWriter, req *http.Request) {
+	rb, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Print("Failed read thread request body", err)
+		return
+	}
 
+	var r pb.MetricReportRequest
+	err = proto.Unmarshal(rb, &r)
+	if err != nil {
+		log.Print("Failed parse request body", err)
+		return
+	}
+	fmt.Println(r.String())
+	response, err := proto.Marshal(&pb.MetricReportResponse{
+		Status: pb.MetricReportResponse_OK,
+	})
+	if err != nil {
+		log.Print("Failed marshal response", err)
+		return
+	}
+	_, err = w.Write(response)
+	if err != nil {
+		log.Print("Failed write response", err)
+		return
+	}
 }
 
 func main() {
 	http.HandleFunc("/report", report)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
